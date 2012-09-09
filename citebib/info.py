3b217b0305aa114bbf877f98f4ea6afa26a7a801
François Boulogne <fboulogne@sciunto.org>
2012-09-09 20:55:07 +0200

    add info file

---
+++ citebib/info.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Gather all information about the software
+"""
+
+NAME = "CiteBib"
+URL = "https://github.com/sciunto/CiteBib"
+LICENSE = "GPLv3+"
+EMAIL = "fboulogne@sciunto.org"
+VERSION = "dev"
+
+SHORT_DESCRIPTION = ""
+
+AUHTORS = ["Francois Boulogne <fboulogne@sciunto.org>"]
+
