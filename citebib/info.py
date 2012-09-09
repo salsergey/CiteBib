70f285eb2b4c5c4d3464781631c5c59862eed06d
François Boulogne <fboulogne@sciunto.org>
2012-09-09 22:20:58 +0200

    add description

--- citebib/info.py
+++ citebib/info.py
@@ -10,7 +10,7 @@ LICENSE = "GPLv3+"
 EMAIL = "fboulogne@sciunto.org"
 VERSION = "dev"
 
-SHORT_DESCRIPTION = ""
+SHORT_DESCRIPTION = "Generate a nice Bibtex or Latex bibliography according to the document content"
 
 AUHTORS = ["Francois Boulogne <fboulogne@sciunto.org>"]
 
