cda02ff9f62572e44d3ef446fe9c06c0656de8d3
François Boulogne <fboulogne@sciunto.org>
2012-09-10 20:29:50 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from citebib import info
+
+setup(
+    name         = 'CiteBib',
+    version      = info.VERSION,
+    url          = info.URL,
+    author       = "Francois Boulogne",
+    license      = info.LICENSE,
+    author_email = info.EMAIL,
+    description  = info.SHORT_DESCRIPTION,
+    packages = ['citebib'],
+    scripts     = ['citebib.py'],
+)
