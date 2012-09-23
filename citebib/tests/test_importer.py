0631eb306fc24fb097fab9e2fb7eb472f27b78a4
François Boulogne <fboulogne sciunto org>
2012-09-23 22:12:12 +0200

    improve tests

--- citebib/tests/test_importer.py
+++ citebib/tests/test_importer.py
@@ -9,11 +9,10 @@ from importer import get_citations
 class TestGetCitations(unittest.TestCase):
 
 
-    def test_citations(self):
+    def test_simple_citations(self):
         
         text = """
         \cite{Foo1999}
-        \cite{Foo1999, Bar2012}
         
         \cite{Here1999} blah \cite{There2012}
 
@@ -22,6 +21,56 @@ class TestGetCitations(unittest.TestCase):
         with open(temp, 'w') as tmp:
             tmp.write(text)
 
-        expected = ['Foo1999', 'Bar2012', 'Here1999', 'There2012']
+        expected = ['Foo1999', 'Here1999', 'There2012']
         result = get_citations(temp)
         self.assertEqual(expected, result)
+
+    def test_multiple_citations(self):
+        text = """
+        \cite{Foo1999, Bar2012}
+        """
+        temp = tempfile.mkstemp()[1] 
+        with open(temp, 'w') as tmp:
+            tmp.write(text)
+
+        expected = ['Foo1999', 'Bar2012']
+        result = get_citations(temp)
+        self.assertEqual(expected, result)
+
+    def test_uniq_citations(self):
+        text = """
+        \cite{Foo1999} 
+        \cite{Bar2012}
+        \cite{Foo1999} 
+        """
+        temp = tempfile.mkstemp()[1] 
+        with open(temp, 'w') as tmp:
+            tmp.write(text)
+
+        expected = ['Foo1999', 'Bar2012']
+        result = get_citations(temp)
+        self.assertEqual(expected, result)
+
+    #def test_commented_citations(self):
+    #    text = """
+    #    I want this one \cite{Foo1999} %but not that one \cite{Bar2000}
+    #    """
+    #    temp = tempfile.mkstemp()[1] 
+    #    with open(temp, 'w') as tmp:
+    #        tmp.write(text)
+
+    #    expected = ['Foo1999']
+    #    result = get_citations(temp)
+    #    self.assertEqual(expected, result)
+
+    #def test_citations_with_option(self):
+    #    text = """
+    #    \cite[option]{Foo1999}
+    #    """
+    #    temp = tempfile.mkstemp()[1] 
+    #    with open(temp, 'w') as tmp:
+    #        tmp.write(text)
+
+    #    expected = ['Foo1999']
+    #    result = get_citations(temp)
+    #    self.assertEqual(expected, result)
