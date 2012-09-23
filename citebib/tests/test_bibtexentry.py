d09fd358a8b2fc909c5956007539880d7973855a
François Boulogne <fboulogne sciunto org>
2012-09-23 22:54:33 +0200

    add unittests

--- citebib/tests/test_bibtexentry.py
+++ citebib/tests/test_bibtexentry.py
@@ -25,3 +25,30 @@ class TestCleanLastName(unittest.TestCase):
         result = clean_last_name(lastname)
         self.assertEqual(expected, result)
 
+
+
+#from bibtexentry import get_authors
+#
+#class TestGetAuthor(unittest.TestCase):
+#
+#    def test_simple_list(self):
+#        authors = [{'name': 'Foo, J', 'id': 'FooJ'}, {'name': 'Bar R', 'id': 'BarR'}, {'name': 'Foobar, D', 'id': 'FoobarD'}]
+#        expected = 'Foo, J. and Bar, R. and Foobar, D.'
+#        result = get_authors(authors)
+#        self.assertEqual(expected, result)
+
+from bibtexentry import get_authors_bibtex
+
+class TestGetAuthor(unittest.TestCase):
+
+    def test_simple_list(self):
+        authors = [{'name': 'Foobar, D', 'id': 'FoobarD'}]
+        expected = 'Foobar, D.'
+        result = get_authors_bibtex(authors)
+        self.assertEqual(expected, result)
+
+    def test_simple_list2(self):
+        authors = [{'name': 'Foo, D', 'id': 'FooD'}, {'name': 'Bar, B', 'id': 'BarB'}]
+        expected = 'Foo, D. and Bar, B.'
+        result = get_authors_bibtex(authors)
+        self.assertEqual(expected, result)
