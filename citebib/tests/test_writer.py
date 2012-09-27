89088d224bb28b49a6ee3c77b2ba109cedcd6752
François Boulogne <fboulogne sciunto org>
2012-09-27 22:08:10 +0200

    first implementation latex IO

---
+++ citebib/tests/test_writer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from writer import print_latex_biblio
+from io import StringIO
+
+class TestLatexBiblio(unittest.TestCase):
+
+    def test_article(self):
+
+        biblio = {'Carroll1986': {'author': [{'id': 'CarrollBJ', 'name': 'Carroll, B J'}],
+                     'journal': {'id': 'Langmuir', 'name': 'Langmuir'},
+                     'pages': '248 to 250', 'title': 'Equilibrium conformations of liquid drops on thin cylinders under forces of capillarity. A theory for the roll-up process', 
+                     'type': 'article', 'volume': '2', 'year': '1986'}}
+       
+        out = StringIO()
+        print_latex_biblio(biblio, out=out)
+        result = out.getvalue().strip()
+
+        expected = 'B. J. Carroll, Langmuir (1986)' 
+        self.assertEqual(result, expected)
--- citebib/writer.py
+++ citebib/writer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 from citebib.bibtexentry import clean_entry
 
 def write_bibtex(entries, filename):
@@ -20,13 +22,19 @@ def write_bibtex(entries, filename):
         output.write(block)
 
 
-def print_latex_biblio(entries):
+def print_latex_biblio(entries, out=sys.stdout):
     """
     Print entries to the latex format
     """
 
+    #TODO should depend on filetype !
+    #Example here for an article...
+    separator = ', '
     for entry in entries:
         line = ''
         line += clean_entry('author', entries[entry]['author'], format='latex')
+        line += separator
+        line += clean_entry('journal', entries[entry]['journal'], format='latex')
+        line += ' (' + clean_entry('year', entries[entry]['year'], format='latex') + ')'
 
-        print(line)
+        out.write(line)
