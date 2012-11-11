867bf52fa97bc0f08427fc5d593c1fcfd01ee57f
François Boulogne <fboulogne sciunto org>
2012-11-11 20:41:22 +0100

    add unittest

--- citebib/tests/test_converter.py
+++ citebib/tests/test_converter.py
@@ -11,6 +11,13 @@ class TestLatexConverter(unittest.TestCase):
         result = string_to_latex(string)
         expected = "\`{a} \\\'{e} \`{e} \\\"{o}"
         self.assertEqual(result, expected)
+
+    def test_accent(self):
+        """ Don't touch { and }"""
+        string = '{A}'
+        result = string_to_latex(string)
+        expected = '{A}'
+        self.assertEqual(result, expected)
         
 from converter import protect_uppercase
 class TestUppercaseProtection(unittest.TestCase):
