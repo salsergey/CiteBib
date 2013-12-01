d79713d0bd209fe3941db6f298810d8d4e1642a4
François Boulogne <fboulogne sciunto org>
2013-12-01 18:51:35 -0500

    bump version

--- libcitebib/info.py
+++ libcitebib/info.py
@@ -23,7 +23,7 @@ NAME = "CiteBib"
 URL = "https://github.com/sciunto/CiteBib"
 LICENSE = "GPLv3+"
 EMAIL = "fboulogne@sciunto.org"
-VERSION = "0.3"
+VERSION = "0.4"
 
 SHORT_DESCRIPTION = "Generate a nice Bibtex or Latex bibliography according to the document content"
 
