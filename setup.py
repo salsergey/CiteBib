471485713326f5889ce8e8404ebe15e9b0d97342
Ioannis Filippidis <jfilippidis@gmail.com>
2016-09-06 17:06:01 -0700

    REF: use `__main__` stanza in `setup.py`

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ from setuptools import setup
 
 from libcitebib import __version__ as version
 
-setup(
-    name         = 'CiteBib',
-    version      = version,
-    url          = "https://github.com/sciunto-org/python-bibtexparser",
-    author       = "Francois Boulogne",
-    license      = "GPLv3+",
-    author_email = "fboulogne@sciunt.org",
-    description  = "Generate a nice Bibtex or Latex bibliography according to the document content",
-    packages     = ['libcitebib'],
-    scripts      = ['citebib', 'citekey'],
-)
+def run_setup():
+    setup(
+        name='CiteBib',
+        version=version,
+        url="https://github.com/sciunto-org/python-bibtexparser",
+        author="Francois Boulogne",
+        license="GPLv3+",
+        author_email="fboulogne@sciunt.org",
+        description=(
+            "Generate a nice Bibtex or Latex bibliography "
+            "according to the document content"),
+        packages=['libcitebib'],
+        scripts=['citebib', 'citekey'],
+
+
+if __name__ == '__main__':
+    run_setup()
