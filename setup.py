622e4237a08ab665632dba3e89ddd6dec6d7a44c
Ioannis Filippidis <jfilippidis@gmail.com>
2016-09-06 17:08:07 -0700

    BUG: define version in `setup.py` and dump to `_version.py`
    because installation fails when `bibtexparser` not yet installed.

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-from libcitebib import __version__ as version
+
+VERSION_FILE = 'libcitebib/_version.py'
+MAJOR = 0
+MINOR = 4
+MICRO = 3
+VERSION = '{major}.{minor}.{micro}'.format(
+    major=MAJOR, minor=MINOR, micro=MICRO)
+VERSION_TEXT = (
+    '# This file was generated from setup.py\n'
+    "version = '{version}'\n")
 
 def run_setup():
+    version = VERSION
+    s = VERSION_TEXT.format(version=version)
+    with open(VERSION_FILE, 'w') as f:
+        f.write(s)
     setup(
         name='CiteBib',
         version=version,
